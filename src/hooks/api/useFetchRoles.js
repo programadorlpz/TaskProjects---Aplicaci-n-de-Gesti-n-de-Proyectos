858aad9642cad3ff6fd51062fb2985544a242170
Edward López <97465512+programadorlpz@users.noreply.github.com>
2024-10-21 18:52:17 -0500

    Update useFetchRoles.js

--- src/hooks/api/useFetchRoles.js
+++ src/hooks/api/useFetchRoles.js
@@ -9,7 +9,7 @@ const useFetchRoles = () => {
   useEffect(() => {
     const fetchRoles = async () => {
       try {
-        const response = await axios.get('https://programadorlpz.github.io/TaskProjects---Aplicaci-n-de-Gesti-n-de-Proyectos/assets/role.json');
+        const response = await axios.get('https://github.com/programadorlpz/TaskProjects---Aplicaci-n-de-Gesti-n-de-Proyectos/blob/gh-pages/public/assets/role.json');
         setRoles(response.data);
       } catch (error) {
         setErrorRoles(error);
